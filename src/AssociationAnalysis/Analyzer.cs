1aea075bbd1ce176e8954c29ceb1f393ed3a0b3e
Chet Parrott <chet.parrott@aclecta.com>
2014-10-23 22:55:53 -0500

    Added initial file loading logic, restructured project, changed to executable console app.

---
+++ src/AssociationAnalysis/Analyzer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AssociationAnalysis.Properties;
+
+namespace AssociationAnalysis
+{
+    public class Analyzer
+    {
+        public static void Main(string[] args)
+        {
+            var data = Encoding.Default
+                               .GetString(Resources.msnbc990928)
+                               .Split('\n')
+                               .Where(d => !d.Contains("%") && d.Trim().Length > 0)
+                               .ToList();
+        }
+    }
+}
