a216d533d8ebd9c5168246b1245993f61c122b66
nhonaitran <ai.tran@nascentlab.com>
2014-11-05 00:42:15 -0600

    Added function to generate binary representation of association data, and save presentation to csv file.  Added misc. tests for added functions.

--- src/AssociationAnalysis/DataSet.cs
+++ src/AssociationAnalysis/DataSet.cs
@@ -7,9 +7,14 @@ namespace AssociationAnalysis
 {
     public class DataSet
     {
+        public int NumberOfTransactions { get; set; }
+        public int NumberOfItems { get; set; }
         public IEnumerable<IEnumerable<int>> SequentialData { get; set; }
         public IEnumerable<IEnumerable<int>> AssociationData { get; set; }
         public Dictionary<int, string> DataHeaders { get; set; }
+
+        public int[][] AssociationDataBinaryFormat { get; set; }
+        
         public DataSet(byte[] p)
         {// parse file
             var rawData = Encoding.Default
@@ -24,10 +29,29 @@ namespace AssociationAnalysis
             rawData[0].Split(' ').ToList().ForEach(u => DataHeaders.Add(i++, u));
             // remove headers from raw data
             rawData.RemoveAt(0);
-            
+            NumberOfItems = DataHeaders.Count;
+
             char[] separators = {' ', '\r'}; //carriage returns and extra space are at the end of every line
             SequentialData = rawData.Select(u => u.Split(separators, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
-            AssociationData = rawData.Select(u => u.Split(separators, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Distinct().OrderBy(n => n));
+            AssociationData = rawData.Select(u => u.Split(separators, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Distinct().OrderBy(x => x));
+            NumberOfTransactions = rawData.Count;
+        }
+
+        public void CreateBinaryRepresentation() {
+            AssociationDataBinaryFormat = new int[NumberOfTransactions][];
+            int i = 0;
+            AssociationData.ToList().ForEach(t => { 
+                AssociationDataBinaryFormat[i] = new int[NumberOfItems];  
+                t.ToList().ForEach(j => AssociationDataBinaryFormat[i][j - 1] = 1); 
+                i++; 
+            });
+        }
+
+        public void SaveBinaryRepresentation(string filename) {
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(filename)) {
+                file.WriteLine(string.Join(",", DataHeaders.Values));
+                AssociationDataBinaryFormat.ToList().ForEach(t => file.WriteLine(string.Join(",", t.ToList())));
+            }
         }
     }
 }
