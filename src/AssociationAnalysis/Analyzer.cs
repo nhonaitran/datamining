10f0f76ef903e8141b4a23506cdd343ba52cb032
Chet Parrott <chet.parrott@aclecta.com>
2014-12-01 00:18:14 -0600

    MDP data generator is included here. It's a very simple frequency counter that is used to create an adjacency matrix, which is fed into excel for cleanup/analysis, which is then fed into a graph visualization software called yEd for further polishing.

--- src/AssociationAnalysis/Analyzer.cs
+++ src/AssociationAnalysis/Analyzer.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
+using System.Text;
 using AssociationAnalysis.Apriori;
 using AssociationAnalysis.Properties;
 
@@ -14,8 +17,8 @@ namespace AssociationAnalysis
                                .ForEach(
                                             u => Console.WriteLine("{0}:{1}", u.Key, u.Value)
                                        );
-
-            Apriori(dataSet);
+            // Apriori(dataSet);
+            // Markov(dataSet);
 
             Console.ReadLine(); //Stop program so we can read output
         }
@@ -27,6 +30,31 @@ namespace AssociationAnalysis
             var miner = new AprioriMiner(cData) { MinSupport = 0.01, MinConfidence = 0.3, Verbose = verbose };
             cData.printInfo();
             miner.mine();
+        }
+
+        private static void Markov(DataSet data)
+        {
+            var previousNode = 0;
+            var nonDistinctNavigations = data.SequentialData.Where(d => d.Count() > 1);
+            var visitMatrix = new int[data.DataHeaders.Count + 1, data.DataHeaders.Count + 1];
+
+            foreach (var node in nonDistinctNavigations.SelectMany(navigationList => navigationList))
+            {
+                visitMatrix[previousNode, node]++;
+                previousNode = node;
+            }
+
+            var stringBuilder = new StringBuilder();
+
+            for (var i = 1; i < data.DataHeaders.Count + 1; i++)
+            {
+                for (var j = 1; j < data.DataHeaders.Count + 1; j++)
+                {
+                    stringBuilder.Append(visitMatrix[i, j] + ",");
+                }
+                Console.WriteLine(stringBuilder.ToString().Trim(','));
+                stringBuilder.Clear();
+            }
         }
     }
 }
