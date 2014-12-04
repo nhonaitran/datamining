9f53609e67894ee06bf851830c2ff345b19afb2e
Ai Tran <ai.tran@nascentlab.com>
2014-12-04 09:06:10 -0600

    Added write-up on association analysis and visualization for project report.

--- R/association.R
+++ R/association.R
@@ -13,19 +13,22 @@ itemFrequency(transactions)
 # plot frequency of each category
 itemFrequencyPlot(transactions, support=0.001)
 
-# find rules that have minimum support and confidence = 1% and 70%
-system.time(rules <- apriori(transactions, parameter = list(supp=0.01, conf=0.30, minlen=2)))
+# find rules that have minimum support and confidence = 1% and 30%, with at least two items
+system.time(rules <- apriori(transactions, parameter = list(supp=0.01, conf=0.01, minlen=2)))
 summary(rules)
 
 # find 2-itemset rules that have minimum support and confidence being 1\% and 35\%
 system.time(rules <- apriori(transactions, parameter = list(supp=0.02, conf=0.35, minlen=2, maxlen=2)))
 
+# find rules with specific items in antecedent set that have minimum support and confidence being 1\% and 1\%
 system.time(rules <- apriori(transactions, parameter = list(supp=0.01, conf=0.01), appearance = list(lhs = c("frontpage","business","local","news","on-air","sports","tech"), default="lhs") ))
 
 # find rules that have rhs=Front page
 system.time(rules <- apriori(transactions, parameter = list(supp=0.01, conf=0.01), appearance = list(lhs = c("frontpage"), default="lhs") )
 
 inspect(sort(rules, by="support"))
+
+# plots rules
 plot(rules, measure="support", shading="lift")
 plot(rules, measure=c("support","lift"), shading="confidence")
 plot(rules, shading="order", control=list(main="Two-key plot"))
