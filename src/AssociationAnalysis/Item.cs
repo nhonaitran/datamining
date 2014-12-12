f2e6a991083c77f2f8711bc082aff139c5322507
nhonaitran <ai.tran@nascentlab.com>
2014-12-12 13:52:14 -0600

    Added Transaction, Item classes.

---
+++ src/AssociationAnalysis/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssociationAnalysis {
+    public class Item {
+        public int ID { get; set; }
+        public string Label { get; set; }
+    }
+}
