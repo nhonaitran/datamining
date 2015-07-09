d5013d7f12acfe752c8b3c23ff55f6f5ddfddde1
Ai Tran <ai.tran@nascentlab.com>
2015-07-09 06:52:38 -0500

    Added python cde for code challenge

---
+++ datacc/src/median_unique.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Usage: python %s <input_file_name> (<output_file_name>)" % sys.argv[0], file=sys.stderr)
+        exit(-1)
+
+    cnt = len(sys.argv)
+    inputFile = sys.argv[1]
+    if cnt < 3:
+        outputFile = os.path.join("..", "tweet_output", "ft2.txt")
+    else:
+        outputFile = sys.argv[2]
+
+    assert os.path.isfile(inputFile), "invalid file: \"%s\"" % inputFile
